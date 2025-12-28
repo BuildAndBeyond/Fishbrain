8eaba6cdfe9b62f450dc0e553682faea19a444f0
BuildAndBeyond <philipp.heinen@gmx.de>
2025-12-28 12:25:05 +0100

    Delete lib directory

--- lib/storage/database_connection_stub.dart
+++
@@ -1,7 +0,0 @@
-import 'package:drift/drift.dart';
-
-LazyDatabase openDatabaseConnection() {
-  return LazyDatabase(() async {
-    throw UnsupportedError('Keine DB-Implementierung für diese Plattform.');
-  });
-}
--- lib/storage/database_connection_web.dart
+++
@@ -1,6 +0,0 @@
-import 'package:drift/drift.dart';
-import 'package:drift/web.dart';
-
-LazyDatabase openDatabaseConnection() {
-  return LazyDatabase(() async => WebDatabase('catchlog_web'));
-}
